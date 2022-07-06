a902e4d180ae881141f7feaf585bcb829164ed9f
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-07-06 16:52:49 +0000

    Create person class and other 2 sub classes

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require './person'
+
+class Teacher < Person
+  def initialize(age, specialization, name: 'Unknown')
+    super(name, age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
